2926c38b132fca8eb1f88a824cf5f79d7ad39ce7
vladdez <dezem@bk.ru>
2023-10-10 10:12:58 +0200

    first commit

---
+++ src/MyStatPackage.jl
@@ -0,0 +1,10 @@
+module MyStatPackage
+
+    include("statistic_functions.jl")
+    export SimulationResults
+    export rse_sum
+    export rse_tstat
+    export rse_std
+    export rse_tstat
+
+end # module MyStatPackage
---
+++ src/statistic_functions.jl
@@ -0,0 +1,35 @@
+function rse_sum(array)
+    +(collect(array)...)
+end
+
+
+function rse_mean(array)
+    a = collect(array)
+    b = +(a...) / length(a)
+    return b
+end
+
+
+function rse_std(array)
+    a = collect(array)
+    aa = a .- rse_mean(array)
+    b = rse_sum(aa.^2) / (length(a) - 1)
+    c = sqrt(b)
+    return c
+end
+
+
+function rse_tstat(array; σ = rse_std(array))
+    x = collect(array)
+    return res = rse_mean(x) / (σ / sqrt(length(x)))
+
+end
+
+
+
+struct StatResult 
+    x::UnitRange
+    n::Int
+    std::Float64
+    tvalue::Float64
+end
